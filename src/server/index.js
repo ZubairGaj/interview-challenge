805d8247d8af2645b19371aa78406b094c44c160
ZubairGaj <21358932+ZubairGaj@users.noreply.github.com>
2023-10-05 09:55:08 +0100

    push changes

--- src/server/index.js
+++ src/server/index.js
@@ -7,6 +7,21 @@ const port = process.env.PORT || 8080;
 
 app.use(express.static('dist'));
 
-app.get('/api/items', (req, res) => res.send({ items }));
+app.get('/api/items', (req, res) => {
+    const responseArray = [];
+
+    if (req.query.search.length < 1) {
+        const responseArray = items;
+        return res.send({ responseArray })
+    }
+
+    items.forEach(element => {
+
+        if (element.name.toLowerCase().includes(req.query.search.toLowerCase())) {
+            responseArray.push(element)
+        }
+    });
+    return res.send({ responseArray })
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
